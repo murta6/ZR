66b071ec0ba432e04575bdb3b71534c43edbcd33
Murta <murta-thelegend@hotmail.com>
2016-06-06 14:05:08 +0200

    Programiranje nastavljeno

--- Aplikacija/Aplikacija/Logika/KonkretnaProvjera.cs
+++ Aplikacija/Aplikacija/Logika/KonkretnaProvjera.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aplikacija.Model;
+using org.mariuszgromada.math.mxparser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +8,69 @@ using System.Threading.Tasks;
 
 namespace Aplikacija.Logika
 {
+
     public class KonkretnaProvjera
     {
+        public List<KonkretanZadatak> zadaci;
+        public double brojBodova { get; }
+        public double ostvareniBrojBodova { get; set; }
 
+        private KonkretnaProvjera(double brojBodova)
+        {
+            this.brojBodova = brojBodova;
+            zadaci = new List<KonkretanZadatak>();
+        }
+
+        public static KonkretnaProvjera generirajProvjeru(List<int> rbrZadataka, double brojBodova)
+        {
+            KonkretnaProvjera provjera = new KonkretnaProvjera(brojBodova);
+            foreach(var rbrZad in rbrZadataka)
+            {
+                var zad = Zadaci.vratiZadatak(rbrZad);
+                KonkretanZadatak zadatak = new KonkretanZadatak(zad.pitanje, zad.izraz, zad.parametri, zad.slika, zad.sifraSlozenosti);
+                provjera.zadaci.Add(zadatak);
+            }
+            return provjera;
+        }
+
+        public void zapisiOdgovore(Double[] odgovori)
+        {
+            if(zadaci.Count != odgovori.Length)
+            {
+                throw new ArgumentException("Nema jednako odgovora koliko pitanja");
+            }
+            int i = 0;
+            foreach(var zad in zadaci)
+            {
+                zad.korisnikovOdgovor = odgovori[i];
+                i++;
+            }
+        }
+
+        public void ocijeniProvjeru()
+        {
+            foreach (var zad in zadaci)
+            {
+                zad.izracunajTocnost();
+            }
+        }
     }
 
     public class KonkretanZadatak
     {
+
+        public static readonly double tolereancija = 5.0;
+
         public String pitanje { get; }
         public String[] parametri { get; }
-        private double odgovor { get; }
+        public double odgovor { get; }
         public byte[] slika { get; }
         public int sifraSlozenosti { get; }
-        private Boolean tocno {get; set; }
+        public Boolean tocno { get; set; }
         public int brojBodova { get; }
         public double negativni { get; }
+        public String izraz { get; }
+        public double korisnikovOdgovor { get; set; }
 
         public KonkretanZadatak(String pitanje, String izraz, String parametri, byte[] slika, int sifraSlozenosti)
         {
@@ -28,21 +78,36 @@ namespace Aplikacija.Logika
             this.parametri = generirajParametre(parametri);
             this.slika = slika;
             this.sifraSlozenosti = sifraSlozenosti;
+            this.izraz = izraz;
+            odgovor = izracunajOdgovor();
         }
 
-        private static String[] generirajParametre(String parametri)
+        private String[] generirajParametre(String parametri)
         {
             String[] par = parametri.Split(' ');
-            foreach(String parametar in par)
+            Random rand = new Random();
+            for (int i = 0; i < par.Length; i++)
             {
-
+                String[] param = par[i].Split(':');
+                var br = rand.NextDouble() * 40;
+                par[i] = param[0] + ":" + br.ToString();
             }
             return par;
         }
 
-        private static void ocijeniOdgovor(String izraz, String[] par)
+        private double izracunajOdgovor()
         {
+            Expression exp = new Expression(izraz);
+            foreach(String param in parametri){
+                var parametarime = param.Split(':');
+                exp.addArguments(new Argument(parametarime[0], Double.Parse(parametarime[1])));
+            }
+            return exp.calculate();
+        }
 
+        public void izracunajTocnost()
+        {
+            tocno = ((odgovor - tolereancija / 100 * odgovor) < korisnikovOdgovor) && ((odgovor + tolereancija / 100 * odgovor) > korisnikovOdgovor);
         }
     }
 }
